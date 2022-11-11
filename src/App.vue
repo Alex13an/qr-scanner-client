34b274f9eecee6f8cfb726858f9d03ffd686206c
Alexandr An <alexandr13an@yandex.ru>
2022-11-11 16:18:40 +0300

    frontenf for guests created

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="content">
+    <router-view />
+  </div>
 </template>
 
+<script>
+export default {};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Myriad Pro;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+:root {
+  --blue: #0077d4;
+  --light-blue: rgba(0, 119, 212, 0.8);
+  --green: #95c11f;
+  --light-green: rgba(149, 193, 3, 0.8);
+  --purple: #9f358b;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,35 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import MainPage from '../pages/MainPage';
+import VolunteersScan from '../pages/VolunteersScan';
+import GuestsEnterScan from '../pages/GuestsEnterScan';
+import GuestsEventsEnterScan from '../pages/GuestsEventsEnterScan';
+import GuestsEventsSelect from '../pages/GuestsEventsSelect';
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: '/',
+    name: 'home',
+    component: MainPage,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: '/scan/volunteers',
+    name: 'volunteers',
+    component: VolunteersScan,
+  },
+  {
+    path: '/scan/guests/enter',
+    name: 'guests',
+    component: GuestsEnterScan,
+  },
+  {
+    path: '/scan/guests/events',
+    name: 'guests-events',
+    component: GuestsEventsSelect,
+  },
+  {
+    path: '/scan/guests/events/:eventId',
+    name: 'event',
+    component: GuestsEventsEnterScan,
   },
 ];
 
