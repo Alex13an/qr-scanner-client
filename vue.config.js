af146b120d5862a4c860842be718d706a6fc3e62
Alexandr An <alexandr13an@yandex.ru>
2022-11-11 22:56:48 +0300

    cors fixed

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  devServer: {
+    proxy: 'https://62.217.179.117:3000',
+  },
 });
